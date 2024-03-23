26b5a2928e57a9d69a06ab07294a4eca7ca45759
Shehab Waleed <shehabwaleedd@gmail.com>
2024-03-23 21:42:59 +0200

    Refactor contact component to use ContactForm component

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from "./page.module.scss"
+import ContactForm from "@/components/contactForm"
+import { BsYoutube, BsInstagram, BsTwitterX, BsTiktok, BsSnapchat } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa6";
+import Link from "next/link";
+import { FaLinkedinIn } from "react-icons/fa";
+const page = () => {
+    return (
+        <main className={styles.contact}>
+            <section className={styles.contact__container}>
+                <Image src="/assets/covers/contact cover.webp" alt="Landing" width={500} height={500} />
+                <div className={styles.contact__container__content}>
+                    <h1>Keep in touch</h1>
+                    <p> Let&apos;s work together to create something unique. </p>
+                    <div className={styles.divider}></div>
+                    <ContactForm />
+                </div>
+            </section>
+            <section className={styles.contact__container__section}>
+                <h2> KEEP IN TOUCH WITH US <br /> ON SOCIAL MEDIA</h2>
+                <div className={styles.contact__container__section_svg}>
+                    <Link href="https://www.facebook.com/"><FaFacebookF /></Link>
+                    <Link href="https://www.instagram.com/"><BsInstagram /></Link>
+                    <Link href="https://www.linkedin.com/"><FaLinkedinIn /></Link>
+                    <Link href="https://www.youtube.com/"><BsYoutube /></Link>
+                    <Link href="https://www.twitter.com/"><BsTwitterX /></Link>
+                    <Link href="https://www.tiktok.com/"><BsTiktok /></Link>
+                    <Link href="https://www.snapchat.com/"><BsSnapchat /></Link>
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default page
--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,37 +1,12 @@
 import React from 'react'
 import styles from "./style.module.scss"
-
+import ContactForm from "@/components/contact"
 const index = () => {
     return (
         <section className={styles.contactHomePage}>
             <h2>Keep In Touch</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, voluptatibus.</p>
-            <div className={styles.contactHomePage__container}>
-                <div className={styles.contactHomePage__container__left}>
-                    <div className={styles.contactHomePage__container__left_header}>
-                        <h3>Address</h3>
-                        <p>225 B Gardenia Street - Hadayk Al-Ahram, First Gate</p>
-                    </div>
-                    <div className={styles.contactHomePage__container__left_header}>
-                        <h3>Call Us</h3>
-                        <a href="tel:201128989894">+201128989894</a>
-                        <a href="tel:0216809142">+0216809142</a>
-                    </div>
-                    <div className={styles.contactHomePage__container__left_header}>
-                        <h3>Email</h3>
-                        <a href="mailto:digital@tagmediaeg.com">
-                            Digital@tagmediaeg.com
-                        </a>
-                    </div>
-                </div>
-                <form className={styles.contactHomePage__container__right}>
-                    <input type="text" placeholder="Name" />
-                    <input type="email" placeholder="Email" />
-                    <input type="text" placeholder="Subject" />
-                    <textarea placeholder="Message"></textarea>
-                    <button>Send Message</button>
-                </form>
-            </div>
+            <ContactForm />
         </section>
     )
 }
